891962f401ef49f508ca316bacb16c68fb6f863e
Chinnathorn P <chinnathorn.p@gmail.com>
2024-06-09 18:26:59 +0700

    Update yt

--- src/page/port.js
+++ src/page/port.js
@@ -43,6 +43,61 @@ const Discography = ({ currentPage, lang, setLang, setPage }) => {
   return (<div style={{ marginTop: 80, marginBottom: 150 }}>
     <CardHeader title={<h3>Discography and Acting of Kaofrang</h3>} subheader={lang == 'th' ? 'ผลงานที่ผ่านมาของน้องข้าวฟ่าง (อ้างอิงจาก Spotify และ Youtube)' : "All Discography and Acting of Kaofrang Yanisa or Kaofrang BNK48 (From Spotify and Youtube)"} />
     <div className='container mt-3'>
+       {data2 != null ? (
+        <Grid container spacing={2}>
+          <CardHeader title={lang == 'th' ? 'ห้องนั่งเล่นของข้าวฟ่าง' : "'Frang Playground"} subheader={lang == 'th' ? 'ผลงานเพลงและคอนเทนท์ของน้องข้าวฟ่าง (อ้างอิงจาก Youtube)' : "All Discography and original contents of Kaofrang Yanisa or Kaofrang BNK48 (From Youtube)"} />
+          {
+            data2.map((item, i) => (
+              <Card component={Grid} className='mb-3 ml-3 ml-lg-0' container key={item.snippet.resourceId.videoId}>
+                <Grid xs={12}>
+                  <CardMedia
+                   sx={{ display: { xs: 'none', md: 'block' }, width: '100%', height: 400 }}
+                    component="iframe"
+                    image={'https://youtube.com/embed/' + item.snippet.resourceId.videoId}
+                    alt={item.snippet.title}
+                  />
+                  <CardMedia
+                   sx={{ display: { xs: 'block', md: 'none' }, width: '100%' }}
+                    component="img"
+                    image={item.snippet.thumbnails.maxres.url}
+                    alt={item.snippet.title}
+                  />
+                </Grid>
+                <Grid item md sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
+                    <Typography component="div" variant="h5" sx={{ fontSize: 22 }}>
+                      <b>{item.snippet.title}</b>
+                    </Typography>
+                    <small className='text-muted'>{lang == 'th' ? 'อัปโหลดโดย ' : "Uploaded by "} {item.snippet.videoOwnerChannelTitle}</small>
+                    <hr />
+                    <Typography variant='p' color="text.primary" className='mt-2' dangerouslySetInnerHTML={{__html: (lang == 'th' ? 'รายละเอียด: ' : "Description: ") + item.snippet.description.replace(/\n/g, "<br />")}}></Typography>
+                    <CardActionArea className='mt-5'>
+                      <Button sx={{ display: { xs: 'block', md: 'none' } }} variant='outlined' className='text-success border-success' onClick={() => window.open('https://youtube.com/watch?v=' + item.snippet.resourceId.videoId, '_blank')}>
+                        {lang == 'th' ? 'รับชมบนยูทูป' : "View on Youtube"}
+                      </Button>
+                      <Button variant='outlined' className='text-primary border-primary mt-2' onClick={() => window.open('https://youtube.com/channel/'+item.snippet.videoOwnerChannelId, '_blank')}>
+                        {lang == 'th' ? 'รับชมรายการอื่นในช่อง ' + item.snippet.videoOwnerChannelTitle : 'View other contents in "' + item.snippet.videoOwnerChannelTitle + '" channel'}
+                      </Button>
+                    </CardActionArea>
+                  </CardContent>
+                </Grid>
+              </Card>
+            ))
+          }
+        </Grid>
+      ) : (
+        <Card>
+          <CardContent>
+            <Skeleton variant="text" className='bg-m' sx={{ fontSize: '2rem' }} />
+            <Skeleton variant="text" className='bg-m' sx={{ fontSize: '1rem' }} />
+            <Skeleton variant="text" className='bg-m' sx={{ fontSize: '1rem' }} />
+            <Skeleton variant="text" className='bg-m' sx={{ fontSize: '1rem' }} />
+            <Skeleton variant="text" className='bg-m' sx={{ fontSize: '1rem' }} />
+            <Skeleton variant="text" className='bg-m' sx={{ fontSize: '1rem' }} />
+          </CardContent>
+        </Card>
+      )}
+      <div className='mt-5' />
       {data1 != null ? (
         <Grid container spacing={2}>
           <CardHeader title={lang == 'th' ? 'แฟรงเพลย์' : "'Frang Play"} subheader={lang == 'th' ? 'ผลงานเพลงน้องข้าวฟ่าง (อ้างอิงจาก Spotify)' : "All Discography and Single of Kaofrang Yanisa or Kaofrang BNK48 (From Spotify)"} />
