400435a75fa197bf9db08341db2cd625c245058d
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-06-27 15:23:23 +0700

    Update frane

--- src/page/_iframe.js
+++ src/page/_iframe.js
@@ -7,13 +7,18 @@ const IFRAMEwithPost = ({item}) => {
     const ref = React.useRef(null);
 
 
-    return ( <iframe src={'https://instagram.com/p/' + item.postId + '/embed'} ref={ref} width="100%"
-    style={{
-        height: 500,
-        overflow: "auto",
-        border: "none"
-    }}></iframe> );
-  
+    // return ( <iframe src={'https://instagram.com/p/' + item.postId + '/embed'} ref={ref} width="100%"
+    // style={{
+    //     height: 500,
+    //     overflow: "auto",
+    //     border: "none"
+    // }}></iframe> );
+    return (
+        <InstagramEmbed url={"https://www.instagram.com/p/" + item.postId} ref={ref} width='100%' height={500} style={{
+            overflow: "auto",
+            border: "none"
+        }} />
+    )
 }
  
 export default IFRAMEwithPost;
