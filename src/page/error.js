df82110e2d985677d9c04f71ce8e9611afc0daec
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-08-08 09:57:18 +0700

    update error

--- src/page/error.js
+++ src/page/error.js
@@ -25,13 +25,8 @@ const Err = ({currentPage, lang, setLang, setPage, setMenu, setLangMod, launch})
         <div>
              <Fade in={true} timeout={1200}>
  <div className="video-container">
-    <div className='d-block d-lg-none img' style={{filter: 'brightness(80%)', backgroundImage: 'url(https://100channel.live/wp-content/uploads/2024/07/kaofrang.jpg)'}}></div>
-    {/* <iframe className='d-none d-lg-block vdo overflow-hidden' src='https://www.youtube.com/embed/tTxaboU2wYQ?autoplay=1&mute=1&controls=0&loop=1&playlist=tTxaboU2wYQ' style={{pointerEvents: 'none', scrollbarWidth: 'none', width: '100vw', height: '100vh'}} playsinline>
-      </iframe> */}
-      <video className='d-none d-lg-block vdo overflow-hidden' muted autoPlay style={{pointerEvents: 'none', scrollbarWidth: 'none'}} playsinline>
-        <source src="https://pub-b590efd174044d4bb32753301d5bbd24.r2.dev/kf1.mp4" type="video/mp4"/>
-        Your browser does not support the video tag.
-      </video>
+    <div className='d-block d-lg-none img' style={{filter: 'brightness(80%)', backgroundImage: 'url(https://ucjgycqgnxeuujucorsm.supabase.co/storage/v1/object/public/kfsite/kaofrang.webp)'}}></div>
+    <div className='d-none d-lg-block img' style={{filter: 'brightness(80%)', backgroundImage: 'url(https://pbs.twimg.com/media/GTqpfrGb0AAcPAG?format=webp&name=4096x4096)'}}></div>
   </div>
  </Fade>
  {data ? (
