fe986ae044cd5e4612435abf89b56a1f8e6542b7
Chinnathorn P <chinnathorn.p@gmail.com>
2024-08-12 19:02:31 +0700

    Update card size

--- src/page/birth.js
+++ src/page/birth.js
@@ -10,16 +10,15 @@ import {
   Avatar,
   Box,
   CircularProgress,
-  Tab,
+  Alert,
   Typography,
-  List,
+  DialogActions,
   Fab,
-  Skeleton,
+  DialogContent,
   Dialog,
   DialogTitle,
-  DialogContent,
   Divider,
-  DialogActions,
+  IconButton,
   TextField,
   CardMedia,
   Backdrop
@@ -33,7 +32,7 @@ import {
   setLaunch,
 } from "../redux/action";
 import moment from "moment";
-import { AddPhotoAlternate, BorderColor, Save, Delete, AspectRatio, PanTool, Edit, Done } from "@mui/icons-material";
+import { AddPhotoAlternate, BorderColor, Save, Delete, AspectRatio, PanTool, Edit, Done, Wallpaper } from "@mui/icons-material";
 import { Editor } from "react-draft-wysiwyg";
 import { EditorState, ContentState, convertToRaw } from "draft-js";
 import { stateFromHTML } from "draft-js-import-html";
@@ -44,6 +43,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { Resizable } from 're-resizable';
 import * as htmlToImage from 'html-to-image';
 import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+import { SketchPicker } from 'react-color'
 
 const Birth = ({
   currentPage,
@@ -58,6 +58,11 @@ const Birth = ({
 }) => {
   const [headedit, setHeadedit] = React.useState(false);
   const [header, setHead] = React.useState('Kaofrang Birthday');
+  const [bg, setBg] = React.useState('#fff');
+  const [bgd, setChangebg] = React.useState(false);
+  const [sizes, setSizescreennotmatch] = React.useState(window.innerWidth < 700);
+  const [headercolor, setHeadcolor] = React.useState('#000');
+
   const [up, setUp] = React.useState(false);
   const cardsuccess = React.useRef(null)
   const [editmode, setEditmode] = React.useState('');
@@ -85,6 +90,9 @@ const Birth = ({
 
   React.useEffect(() => {
     RefreshDate();
+    window.addEventListener('resize', function (event) {
+      setSizescreennotmatch(window.innerWidth < 700)
+    }, true);
     setPage(lang == "th" ? "กิจกรรมวันเกิดของข้าวฟ่าง" : "Birthday Campaign of Kaofrang");
   }, []);
 
@@ -93,6 +101,7 @@ const Birth = ({
     setAddText([...text, {
       id: API,
       txt: '',
+      color: '#000',
       w: 270,
       h: '100%'
     }])
@@ -103,6 +112,17 @@ const Birth = ({
     let updatedList = text.map(item => {
       if (item.id == id) {
         return { ...item, txt: e.target.value }; //gets everything that was already in item, and updates "done"
+      }
+      return item; // else return unmodified item 
+    });
+
+    setAddText(updatedList);
+  }
+
+  const UpdateColorBody = (color, id) => {
+    let updatedList = text.map(item => {
+      if (item.id == id) {
+        return { ...item, color: color.hex }; //gets everything that was already in item, and updates "done"
       }
       return item; // else return unmodified item 
     });
@@ -130,10 +150,6 @@ const Birth = ({
 
     setAddImg(updatedList);
   }
-
-  React.useEffect(() => {
-    console.log(editmodeimg)
-  }, [editmodeimg])
 
   const addImg = () => {
     const input = document.createElement('input');
@@ -169,6 +185,7 @@ const Birth = ({
       return
     }
     setLoad(true);
+
     toJpeg(cardsuccess.current, { preferredFontFormat: 'QGZvbnQtZmFjZXtuYW1lOidtaXNhbnMnO3NyYzp1cmwoJ2h0dHBzOi8vY2RuLmpzZGVsaXZyLm5ldC9naC9jcHgyMDE3L21pc2Fuc2ZvbnRAbWFpbi9lbi9NaVNhbnMtTm9ybWFsLndvZmYyJykgZm9ybWF0KCd3b2ZmMicpLHVybCgnaHR0cHM6Ly9jZG4uanNkZWxpdnIubmV0L2doL2NweDIwMTcvbWlzYW5zZm9udEBtYWluL2VuL01pU2Fucy1Ob3JtYWwud29mZicpIGZvcm1hdCgnd29mZicpO2ZvbnQtd2VpZ2h0OjUwMDtmb250LXN0eWxlOm5vcm1hbDtmb250LWRpc3BsYXk6c3dhcH0=' })
       .then((dataUrl) => {
         const link = document.createElement('a')
@@ -194,6 +211,9 @@ const Birth = ({
 
   return (
     <Box sx={{ marginTop: { xs: 0, md: 13 }, marginBottom: 15 }}>
+      {
+        sizes && up && <Alert sx={{ position: 'fixed', zIndex: 1300, buttom: '20%' }} onClick={() => setSizescreennotmatch(false)} severity="warning"><b>{lang == 'th' ? 'ขนาดหน้าจอไม่เหมาะสม' : 'The screen size is not appropriate.'}</b> {lang == 'th' ? 'การสร้างการ์ดอาจไม่ได้ขนาดตามที่ระบบกำหนด (ขนาดเท่าบัตรเครดิต)' : 'Your Card maybe not same as standard size (Size similiar to Credit Card)'}</Alert>
+      }
       <CardHeader
         title={<h3>KaofrangFie Day Card</h3>}
         subheader={
@@ -225,20 +245,31 @@ const Birth = ({
               ? "ยังไม่พร้อมให้บริการในขณะนี้ ขออภัยในความไม่สะดวก"
               : "Oops! We are not ready right now."
         }
-        action={headedit ? null : <Button variant="contained" onClick={() => text.length > 0 || img.length > 0 && Export()}>{lang == "th"
-          ? "ส่งออกเป็นรูปภาพ"
-          : "Export to Image"}</Button>}
+        action={headedit ? null : <Box>
+          <Button variant="contained" onPointerUp={() => text.length > 0 || img.length > 0 ? Export() : Swal.fire({
+            title: "No Content",
+            text: lang == "th"
+              ? "คุณสามารถบอกอะไรให้น้องข้าวฟ่างรับรู้ได้นะ"
+              : "Let you can say something to Kaofrang.",
+            icon: "warning"
+          })}>{lang == "th"
+            ? "ส่งออกเป็นรูปภาพ"
+            : "Export to Image"}</Button>
+        </Box>}
       />
       <div className="container d-flex justify-content-center mt-3">
-        <Card sx={{ minHeight: 800, width: { md: 600, xs: '100%' } }} ref={cardsuccess}>
+        <Card sx={{ fontFamily: 'misans', backgroundColor: bg, wordBreak: 'break-all', minHeight: 1011, width: { md: 638, xs: '100%' } }} ref={cardsuccess}>
           <CardContent>
             {
               headedit == false ? (
-                <CardHeader title={<h3>{header}</h3>} action={open ? null : <Button onClick={() => setHeadedit(true)}><Edit /></Button>} />
+                <CardHeader title={<h2 style={{ color: headercolor }}>{header}</h2>} action={open ? null : <Button onClick={() => setHeadedit(true)}><Edit /></Button>} />
               ) : (
                 <Box className='mb-3'>
                   <TextField sx={{ width: '80%' }} value={header} onChange={(e) => setHead(e.target.value)} />
                   <Button className="mt-2" onClick={() => setHeadedit(false)}><Done /></Button>
+                  <br />
+                  <SketchPicker width='40%' color={headercolor}
+                    onChangeComplete={(c) => setHeadcolor(c.hex)} />
                 </Box>
               )
             }
@@ -258,26 +289,40 @@ const Birth = ({
                         resizeMode(item.w + d.width, item.h + d.width, item.id);
                       }}
                     >
-                      <TextField fullWidth multiline rows={5} onChange={(e) => Updateh(e, item.id)} autoComplete="off" value={item.txt} />
                       {!open && (
-                        <div className="col-12">
-                          <Button onClick={() => item.txt.length > 0 && setEditmode('')}>
+                        <div className="col-12 text-right">
+                          <IconButton color='primary' onClick={() => item.txt.length > 0 && setEditmode('')}>
                             <Save />
-                          </Button>
-                          <Button onClick={() => {
-                            setAddText([
-                              ...text.slice(0, i),
-                              ...text.slice(i + 1)
-                            ])
-                            setEditmode('');
+                          </IconButton>
+                          <IconButton color='primary' onClick={() => {
+                            Swal.fire({
+                              title: lang == "th"
+                                ? "คุณต้องการลบข้อความนี้หรือไม่"
+                                : "Do you want to remove this paragraph",
+                              showDenyButton: true,
+                              confirmButtonText: lang == 'th' ? 'ยืนยัน' : "Confirm",
+                              denyButtonText: lang == 'th' ? 'แก้ไขต่อ' : "Stay edit"
+                            }).then((result) => {
+                              /* Read more about isConfirmed, isDenied below */
+                              if (result.isConfirmed) {
+                                setAddText([
+                                  ...text.slice(0, i),
+                                  ...text.slice(i + 1)
+                                ])
+                                setEditmode('');
+                              }
+                            });
                           }}>
                             <Delete />
-                          </Button>
+                          </IconButton>
                         </div>
                       )}
+                      <TextField fullWidth multiline rows={5} onChange={(e) => Updateh(e, item.id)} autoComplete="off" value={item.txt} />
+                      <SketchPicker width='90%' color={item.color}
+                        onChangeComplete={(c) => UpdateColorBody(c, item.id)} />
                     </Resizable>
                   ) : (
-                    <Typography style={{ wordBreak: 'break-all' }} onDoubleClick={() => setEditmode(item.id)} variant="div">{RenderHTML(item.txt.replaceAll('\n', '<br/>'))}</Typography>
+                    <Typography sx={{ fontSize: 24, wordBreak: 'break-all', color: item.color }} onDoubleClick={() => setEditmode(item.id)} variant="div">{RenderHTML(item.txt.replaceAll('\n', '<br/>'))}</Typography>
                   ))
                 }
                 {
@@ -294,22 +339,34 @@ const Birth = ({
                         resizeModeImg(item.w + d.width, item.h + d.width, item.id);
                       }}
                     >
-                      <CardMedia sx={{ width: '100%', height: '100%' }} component='img' src={item.src} />
                       {!open && (
-                        <div className="col-12">
-                          <Button onPointerDown={() => switchimg(item.id)}>
+                        <div className="col-12 text-right">
+                          <IconButton color='primary' onPointerUp={() => switchimg(item.id)}>
                             {editmodeimg !== '' ? <AspectRatio /> : <PanTool />}
-                          </Button>
-                          <Button onPointerDown={() => {
-                            setAddImg([
-                              ...img.slice(0, i),
-                              ...img.slice(i + 1)
-                            ])
+                          </IconButton>
+                          <IconButton color='primary' onPointerUp={() => {
+                            Swal.fire({
+                              title: lang == "th"
+                                ? "คุณต้องการลบรูปนี้หรือไม่"
+                                : "Do you want to remove this image",
+                              showDenyButton: true,
+                              confirmButtonText: lang == 'th' ? 'ยืนยัน' : "Confirm",
+                              denyButtonText: lang == 'th' ? 'แก้ไขต่อ' : "Stay edit"
+                            }).then((result) => {
+                              /* Read more about isConfirmed, isDenied below */
+                              if (result.isConfirmed) {
+                                setAddImg([
+                                  ...img.slice(0, i),
+                                  ...img.slice(i + 1)
+                                ])
+                              }
+                            });
                           }}>
                             <Delete />
-                          </Button>
+                          </IconButton>
                         </div>
                       )}
+                      <CardMedia sx={{ width: '100%', height: '100%' }} component='img' src={item.src} />
                     </Resizable>
                   ))
                 }
@@ -326,11 +383,25 @@ const Birth = ({
               sx={
                 {
                   display: {
+                    bottom: 310,
+                    right: 8,
+                    position: "fixed",
+                    zIndex: 1300
+                  }
+                }
+              }
+              onClick={() => setChangebg(true)}>
+              <Wallpaper />
+            </Fab>
+            <Fab
+              color="primary"
+              sx={
+                {
+                  display: {
                     bottom: 240,
                     right: 8,
                     position: "fixed",
-                    zIndex: 1300,
-                    opacity: opacity,
+                    zIndex: 1300
                   }
                 }
               }
@@ -345,8 +416,7 @@ const Birth = ({
                     bottom: 170,
                     right: 8,
                     position: "fixed",
-                    zIndex: 1300,
-                    opacity: opacity,
+                    zIndex: 1300
                   }
                 }
               }
@@ -362,6 +432,20 @@ const Birth = ({
       >
         <CircularProgress />
       </Backdrop>
+      <Dialog
+        open={bgd}
+      >
+        <DialogTitle>
+          {lang == 'th' ? 'เปลี่ยนสีพื้นหลังการ์ด' : 'Change Card Background Color'}
+        </DialogTitle>
+        <DialogContent>
+          <SketchPicker width='90%' color={bg}
+            onChangeComplete={(c) => setBg(c.hex)} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setChangebg(false)}>{lang == 'th' ? 'ปิด' : 'Close'}</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
