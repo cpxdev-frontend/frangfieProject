61ddfd43d6fb11ffd8a1ef72d2a7c3a9409c1bba
Chinnathorn P <chinnathorn.p@gmail.com>
2024-08-13 22:26:05 +0700

    Update text in eng

--- src/page/donate.js
+++ src/page/donate.js
@@ -50,7 +50,7 @@ const Donate = ({ currentPage, lang, setLang, setPage, launch }) => {
     generatePayload("004999166938497", {})
   );
   React.useEffect(() => {
-    setPage(lang == "th" ? "การโดเนทเพื่อดันข้าวฟ่าง" : "Donate Kaofrang");
+    setPage(lang == "th" ? "โดเนทเพื่อข้าวฟ่าง" : "Donate Kaofrang");
   }, []);
 
   return (
@@ -59,20 +59,20 @@ const Donate = ({ currentPage, lang, setLang, setPage, launch }) => {
         title={<h3>Fight for Kaofrang</h3>}
         subheader={
           lang == "th"
-            ? "ร่วมโดเนทเพื่อผลักดันโปรเจคต่างๆของข้าวฟ่างได้ที่ช่องทางนี้"
-            : "Let's support Kaofrang with our fandom."
+            ? "ร่วมโดเนทเพื่อผลักดันกิจกรรมหรือโปรเจคต่างๆ ของข้าวฟ่างได้ที่ช่องทางนี้"
+            : "Let's support Kaofrang with donate every projects to our fan supporter."
         }
       />
       <div className="container mt-3 d-flex justify-content-center">
         <div className="row text-center">
           <Typography className="col-12">
-            สแกน QR Code ด้านล่างนี้เพื่อโดเนท (รองรับทุกธนาคาร)
+            {lang == 'th' ? 'สแกน QR Code ด้านล่างนี้เพื่อโดเนท (รองรับทุกธนาคาร)' : 'Scan below QR Code to donating (Support every thai mobile banking or some e-wallet applications)'}
           </Typography>
           <div className="col-12 d-flex justify-content-center mt-3">
             <CardMedia
               sx={{ width: 250, height: 250 }}
               src={
-                "https://api.qrserver.com/v1/create-qr-code/?size=600x600&data=" +
+                "https://quickchart.io/qr?size=600&text=" +
                 qrCode
               }
               component="img"
@@ -80,31 +80,56 @@ const Donate = ({ currentPage, lang, setLang, setPage, launch }) => {
           </div>
           <TextField
             select
-            label="เลือกจำนวนเงิน"
+            label={
+              lang == "th"
+                ? "เลือกจำนวนเงิน (บาท)"
+                : "Choose amount (Thai Baht)"
+            }
             className="mt-5 mb-3"
             defaultValue={0}
             fullWidth
-            onChange={(e) => parseInt(e.target.value) == 0 ? setqrCode(generatePayload("004999166938497", {})) : setqrCode(generatePayload("004999166938497", {amount:parseInt(e.target.value)}))}
+            onChange={(e) => parseInt(e.target.value) == 0 ? setqrCode(generatePayload("004999166938497", {})) : setqrCode(generatePayload("004999166938497", { amount: parseInt(e.target.value) }))}
             SelectProps={{
               native: true,
             }}>
-            <option value={0}>ระบุจำนวนเงินเองในภายหลัง</option>
-            <option value={100.0}>20 บาท</option>
-            <option value={100.0}>50 บาท</option>
-            <option value={100.0}>100 บาท</option>
-            <option value={200.0}>200 บาท</option>
-            <option value={300.0}>300 บาท</option>
-            <option value={400.0}>400 บาท</option>
-            <option value={500.0}>500 บาท</option>
-            <option value={1000.0}>1,000 บาท</option>
-            <option value={5000.0}>5,000 บาท</option>
-            <option value={10000.0}>10,000 บาท</option>
+            <option value={0}>{
+              lang == "th"
+                ? "ระบุจำนวนเงินเองในภายหลัง"
+                : "Depended to your choice (Select your amount on your application)"
+            }</option>
+            <option value={100.0}>20</option>
+            <option value={100.0}>50</option>
+            <option value={100.0}>100</option>
+            <option value={200.0}>200</option>
+            <option value={300.0}>300</option>
+            <option value={400.0}>400</option>
+            <option value={500.0}>500</option>
+            <option value={1000.0}>1,000</option>
+            <option value={5000.0}>5,000</option>
+            <option value={10000.0}>10,000</option>
           </TextField>
+          <Button variant="outlined" onClick={() => {
+            const link = document.createElement("a");
+            link.download = "KaofrangFie QR Donate.jpg";
+            link.target = '_blank'
+            link.href = "https://quickchart.io/qr?size=600&text=" +
+              qrCode;
+            link.click();
+          }}>{lang == 'th' ? 'บันทึก QR Code นี้' : 'Save this QR Payment'}</Button>
           <Divider />
-          <Typography className="col-12 mt-3">
-            หรือโอนเข้าบัญชี<b>ธนาคารกสิกรไทย</b> เลขที่บัญชี{" "}
-            <b>176-1-39401-7</b> ชื่อบัญชี{" "}
-            <b>นายคมกฤษ ถาวรชีวัน และ นาย อนุชิต ชาอุรัมย์</b>
+          {lang == 'th' ? (
+            <Typography className="col-12 mt-3">
+              หรือโอนเข้าบัญชี<b>ธนาคารกสิกรไทย</b> เลขที่บัญชี{" "}
+              <b>176-1-39401-7</b> ชื่อบัญชี{" "}
+              <b>นายคมกฤษ ถาวรชีวัน และ นาย อนุชิต ชาอุรัมย์</b>
+            </Typography>
+          ) : (
+            <Typography className="col-12 mt-3">
+              Sorry, but this donation way is supported for thai peoples only.
+            </Typography>
+          )}
+          <Typography variant="subtitle2" className="col-12 mt-3">
+            {lang == 'th' ? 'หมายเหตุ: เงินที่โอนผ่านช่องทางนี้จะถูกโอนเข้าบัญชีของบ้านข้าวฟ่างโดยตรงเพื่อนำไปใช้ในการสนับสนุนในทุกกิจกรรมน้องข้าวฟ่าง โดยเว็บไซต์นี้เป็นแค่เพียงตัวกลางเพื่อกระจายข่าวสารเท่านั้น' : 'Notes: All donated amounts will be transfer directly to Kaofrang\'s Fandom supporter to make supporting about projects of Kaofrang Yanisa or Kaofrang BNK48. This website is only a medium for sharing events or about all of her projects.'}
           </Typography>
         </div>
       </div>
