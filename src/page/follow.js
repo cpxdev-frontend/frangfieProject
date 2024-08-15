2d7309e54bf53253560168d879242559a581cd0c
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-08-15 15:28:24 +0700

    Update follow ui

--- src/page/follow.js
+++ src/page/follow.js
@@ -29,6 +29,7 @@ import {
   faTiktok,
   faLine,
   faXTwitter,
+  faFacebookF,
 } from "@fortawesome/free-brands-svg-icons";
 import {
   faMobileAlt,
@@ -68,168 +69,184 @@ const Follow = ({ currentPage, lang, setLang, setPage, launch }) => {
               : "Follow Kaofrang Yanisa or Kaofrang BNK48 to see her update below."
           }
         />
-        <div className="container mt-3 d-flex justify-content-center">
-          <List
-            sx={{
-              width: "100%",
-              maxWidth: 600,
-              bgcolor: "background.paper",
-              borderRadius: 5,
-            }}>
-            <ListItemButton
-              component="a"
-              href="https://facebook.com/bnk48official.kaofrang"
-              target="_blank"
-              data-aos="fade-right">
-              <ListItemAvatar>
-                <Avatar sx={{ backgroundColor: "#1877F2" }}>
-                  <FontAwesomeIcon icon={faFacebook} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="Facebook Fanpage"
-                secondary="Kaofrang BNK48"
+        <div className="container mt-3 justify-content-center">
+          <div className="row">
+            <div className="col-md col-12 m-xs-2">
+              <CardHeader
+                title={lang == "th" ? "ช่องทางหลัก" : "Kaofrang's Official SNS"}
               />
-            </ListItemButton>
-            <ListItemButton
-              component="a"
-              href="https://instagram.com/kaofrang.bnk48official"
-              target="_blank"
-              data-aos="fade-right"
-              data-aos-delay={window.innerHeight > 900 ? "500" : "0"}>
-              <ListItemAvatar>
-                <Avatar
-                  sx={{
-                    background:
-                      "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
-                  }}>
-                  <FontAwesomeIcon icon={faInstagram} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="Instagram Account"
-                secondary="kaofrang.bnk48official"
+              <List
+                sx={{
+                  width: "100%",
+                  maxWidth: 600,
+                  bgcolor: "background.paper",
+                  borderRadius: 5,
+                }}>
+                <ListItemButton
+                  component="a"
+                  href="https://facebook.com/bnk48official.kaofrang"
+                  target="_blank"
+                  data-aos="fade-right">
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#1877F2" }}>
+                      <FontAwesomeIcon icon={faFacebook} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Facebook Fanpage"
+                    secondary="Kaofrang BNK48"
+                  />
+                </ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href="https://instagram.com/kaofrang.bnk48official"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerHeight > 800 ? "500" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{
+                        background:
+                          "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
+                      }}>
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Instagram Account"
+                    secondary="kaofrang.bnk48official"
+                  />
+                </ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href="https://tiktok.com/@kaofrang.bnk48official"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerHeight > 800 ? "1000" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#000", color: "#fff" }}>
+                      <FontAwesomeIcon icon={faTiktok} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="TikTok Account"
+                    secondary="@kaofrang.bnk48official"
+                  />
+                </ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href="https://www.bnk48.com/index.php?page=listMembers&memberId=86"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerHeight > 800 ? "1500" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#CB96C2" }}>
+                      <FontAwesomeIcon icon={faGlobe} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="BNK48 Official Website"
+                    secondary="Kaofrang"
+                  />
+                </ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href="https://app.bnk48.com/members/bnk48/kaofrang"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerHeight > 800 ? "2000" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#8AAEB5" }}>
+                      <FontAwesomeIcon icon={faMobileAlt} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="IAM48 Mobile Application"
+                    secondary="Kaofrang"
+                  />
+                </ListItemButton>
+              </List>
+            </div>
+            <div className="col-md col-12 m-xs-2">
+              <CardHeader
+              sx={{marginTop: {xs: 8, md:0}}}
+                title={lang == "th" ? "สำหรับชาวกอฟ่าง" : "For Kaofrang Fandom"}
               />
-            </ListItemButton>
-            <ListItemButton
-              component="a"
-              href="https://tiktok.com/@kaofrang.bnk48official"
-              target="_blank"
-              data-aos="fade-right"
-              data-aos-delay={window.innerHeight > 900 ? "1000" : "0"}>
-              <ListItemAvatar>
-                <Avatar sx={{ backgroundColor: "#000", color: "#fff" }}>
-                  <FontAwesomeIcon icon={faTiktok} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="TikTok Account"
-                secondary="@kaofrang.bnk48official"
-              />
-            </ListItemButton>
-            <ListItemButton
-              component="a"
-              href="https://www.bnk48.com/index.php?page=listMembers&memberId=86"
-              target="_blank"
-              data-aos="fade-right"
-              data-aos-delay={window.innerHeight > 900 ? "1500" : "0"}>
-              <ListItemAvatar>
-                <Avatar sx={{ backgroundColor: "#CB96C2" }}>
-                  <FontAwesomeIcon icon={faGlobe} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="BNK48 Official Website"
-                secondary="Kaofrang"
-              />
-            </ListItemButton>
-            <ListItemButton
-              component="a"
-              href="https://app.bnk48.com/members/bnk48/kaofrang"
-              target="_blank"
-              data-aos="fade-right"
-              data-aos-delay={window.innerHeight > 900 ? "2000" : "0"}>
-              <ListItemAvatar>
-                <Avatar sx={{ backgroundColor: "#8AAEB5" }}>
-                  <FontAwesomeIcon icon={faMobileAlt} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="IAM48 Mobile Application"
-                secondary="Kaofrang"
-              />
-            </ListItemButton>
-            <ListItemButton
-              component="a"
-              href="https://cp-bnk48.pages.dev/member/kaofrang"
-              target="_blank"
-              data-aos="fade-right"
-              data-aos-delay={window.innerHeight > 900 ? "2500" : "0"}>
-              <ListItemAvatar>
-                <Avatar sx={{ backgroundColor: "#CB96C2" }}>
-                  <FontAwesomeIcon icon={faDesktop} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="BNK48 Fan Space Platform"
-                secondary="Kaofrang"
-              />
-            </ListItemButton>
-          </List>
-        </div>
-
-        <CardHeader
-          className="mt-5"
-          title={<h3>Let's joining us!</h3>}
-          subheader={
-            lang == "th"
-              ? "เข้าร่วมชมรมคนรักข้าวฟ่างได้ทุกช่องทาง ดังนี้"
-              : "Join the fandom of Kaofrang below."
-          }
-        />
-        <div className="container mt-3 d-flex justify-content-center">
-          <List
-            sx={{
-              width: "100%",
-              maxWidth: 600,
-              bgcolor: "background.paper",
-              borderRadius: 5,
-            }}>
-            <ListItemButton
-              component="a"
-              href="https://facebook.com/105487801630676"
-              target="_blank"
-              data-aos="fade-right">
-              <ListItemAvatar>
-                <Avatar sx={{ backgroundColor: "#1877F2" }}>
-                  <FontAwesomeIcon icon={faFacebook} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="Facebook Fanpage"
-                secondary="Kaofrang BNK48 Thailand Fanclub"
-              />
-            </ListItemButton>
-            <ListItemButton
-              component="a"
-              href="https://instagram.com/kaofrangbnk48.thfc"
-              target="_blank"
-              data-aos="fade-right">
-              <ListItemAvatar>
-                <Avatar
-                  sx={{
-                    background:
-                      "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
-                  }}>
-                  <FontAwesomeIcon icon={faInstagram} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="Instagram Account"
-                secondary="kaofrangbnk48.thfc"
-              />
-            </ListItemButton>
-            {/* <ListItemButton
+              <List
+                sx={{
+                  width: "100%",
+                  maxWidth: 600,
+                  bgcolor: "background.paper",
+                  borderRadius: 5,
+                }}>
+                <ListItemButton
+                  component="a"
+                  href="https://cp-bnk48.pages.dev/member/kaofrang"
+                  target="_blank"
+                  data-aos="fade-right">
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#fb61ee" }}>
+                      <FontAwesomeIcon icon={faDesktop} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="BNK48 Fan Space Platform"
+                    secondary="Kaofrang"
+                  />
+                </ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href="https://facebook.com/105487801630676"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerWidth > 800 ? "500" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#1877F2" }}>
+                      <FontAwesomeIcon icon={faFacebook} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Facebook Fanpage"
+                    secondary="Kaofrang BNK48 Thailand Fanclub"
+                  />
+                </ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href="https://www.facebook.com/groups/280412833494438"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerWidth > 800 ? "1000" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#1877F2" }}>
+                      <FontAwesomeIcon icon={faFacebookF} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Facebook Group"
+                    secondary="กอข้าวของKaofrangBNK48"
+                  />
+                </ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href="https://instagram.com/kaofrangbnk48.thfc"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerWidth > 800 ? "1500" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{
+                        background:
+                          "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
+                      }}>
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Instagram Account"
+                    secondary="kaofrangbnk48.thfc"
+                  />
+                </ListItemButton>
+                {/* <ListItemButton
               component="a"
               href="https://tiktok.com/@kaofrang.bnk48official"
               target="_blank"
@@ -244,37 +261,41 @@ const Follow = ({ currentPage, lang, setLang, setPage, launch }) => {
                 secondary="@kaofrang.bnk48official"
               />
             </ListItemButton> */}
-            <ListItemButton
-              component="a"
-              href="https://x.com/Kaofrangbnk48TH"
-              target="_blank"
-              data-aos="fade-right">
-              <ListItemAvatar>
-                <Avatar sx={{ backgroundColor: "#14171A" }}>
-                  <FontAwesomeIcon icon={faXTwitter} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="X (Twitter)"
-                secondary="@Kaofrangbnk48TH"
-              />
-            </ListItemButton>
-            <ListItemButton
-              component="a"
-              href="https://line.me/ti/g2/_tY9YerBmKlDxIoFzU1zdg"
-              target="_blank"
-              data-aos="fade-right">
-              <ListItemAvatar>
-                <Avatar sx={{ backgroundColor: "#06c755" }}>
-                  <FontAwesomeIcon icon={faLine} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="LINE Open Chat"
-                secondary="Kaofrang BNK48 TH.FC (กอข้าวของข้าวฟ่าง)"
-              />
-            </ListItemButton>
-          </List>
+                <ListItemButton
+                  component="a"
+                  href="https://x.com/Kaofrangbnk48TH"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerWidth > 800 ? "2000" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#14171A" }}>
+                      <FontAwesomeIcon icon={faXTwitter} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="X (Twitter)"
+                    secondary="@Kaofrangbnk48TH"
+                  />
+                </ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href="https://line.me/ti/g2/_tY9YerBmKlDxIoFzU1zdg"
+                  target="_blank"
+                  data-aos="fade-right"
+                  data-aos-delay={window.innerWidth > 800 ? "2500" : "0"}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ backgroundColor: "#06c755" }}>
+                      <FontAwesomeIcon icon={faLine} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="LINE Open Chat"
+                    secondary="Kaofrang BNK48 TH.FC (กอข้าวของข้าวฟ่าง)"
+                  />
+                </ListItemButton>
+              </List>
+            </div>
+          </div>
         </div>
       </Box>
     </Fade>
