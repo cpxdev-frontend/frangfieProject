4b705458377e2254d279287ac80a745467839dbf
CPXDev <cpxdev@outlook.com>
2024-10-29 21:36:43 +0700

    update ping

--- src/page/port.js
+++ src/page/port.js
@@ -156,7 +156,8 @@ const Discography = ({ currentPage, lang, setLang, setPage, guide }) => {
 
   React.useEffect(() => {
     if (clip != null && navigator.connection != undefined) {
-      if (navigator.connection.rtt >= 700) {
+      alert(navigator.connection.downlink)
+      if (navigator.connection.downlink < 3) {
         Swal.fire({
           title: 'This content maybe consume your data usage.',
           text:
