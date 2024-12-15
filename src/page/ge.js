d0c95dd13abf6805137be638a224db7531d3299d
CPXDev <cpxdev@outlook.com>
2024-12-15 21:13:11 +0700

    Update ge detail

--- src/page/ge.js
+++ src/page/ge.js
@@ -5,6 +5,7 @@ import {
   Card,
   CardContent,
   LinearProgress,
+  CardActions,
   CardHeader,
   Button,
   Grid,
@@ -166,8 +167,8 @@ const Ge = ({
     );
     setPage(
       lang == "th"
-        ? "กิจกรรมวันเกิดของข้าวฟ่าง"
-        : "Birthday Campaign of Kaofrang"
+        ? "กิจกรรมการเลือกตั้งเซมบัดซึทั่วไปของ BNK48 และ CGM48 ครั้งที่ 5"
+        : "BNK48 & CGM48 Senbatsu General Election 2025"
     );
   }, []);
 
@@ -381,18 +382,22 @@ const Ge = ({
     <Fade in={open} timeout={300}>
       <Box sx={{ marginTop: { xs: 0, md: 13 }, marginBottom: 15 }}>
         <CardHeader
-          data-tour="birthmain-1"
           title={<h3>BNK48 & CGM48 Senbatsu General Election 2025</h3>}
         />
 
         <Box className="m-3">
-          <Stepper orientation="vertical">
+          <CardHeader title="Event TimeLine" />
+          <Stepper
+            orientation={window.innerWidth > 900 ? "landscape" : "vertical"}
+          >
             <Step
               active={getsessionactive(0)}
-              completed={getsessioncomplete(0)}>
+              completed={getsessioncomplete(0)}
+            >
               <StepLabel
                 StepIconComponent={ScheduleIcon}
-                sx={{ backgroundColor: timeline > 0 ? "#58eb34" : "" }}>
+                sx={{ backgroundColor: timeline > 0 ? "#58eb34" : "" }}
+              >
                 <h6>
                   {lang == "th"
                     ? "เปิดลงทะเบียนการเข้าร่วมกิจกรรม (สำหรับเมมเบอร์ BNK48 และ CGM48)"
@@ -407,10 +412,12 @@ const Ge = ({
             </Step>
             <Step
               active={getsessionactive(1)}
-              completed={getsessioncomplete(1)}>
+              completed={getsessioncomplete(1)}
+            >
               <StepLabel
                 StepIconComponent={HowToVoteIcon}
-                sx={{ backgroundColor: timeline > 1 ? "#58eb34" : "" }}>
+                sx={{ backgroundColor: timeline > 1 ? "#58eb34" : "" }}
+              >
                 <h6>{lang == "th" ? "เปิดการโหวต" : "Voting Period"}</h6>
               </StepLabel>
               <StepContent>
@@ -421,10 +428,12 @@ const Ge = ({
             </Step>
             <Step
               active={getsessionactive(2)}
-              completed={getsessioncomplete(2)}>
+              completed={getsessioncomplete(2)}
+            >
               <StepLabel
                 StepIconComponent={PollIcon}
-                sx={{ backgroundColor: timeline > 2 ? "#58eb34" : "" }}>
+                sx={{ backgroundColor: timeline > 2 ? "#58eb34" : "" }}
+              >
                 <h6>
                   {lang == "th"
                     ? "ประกาศผลด่วน 24 ชั่วโมงแรก"
@@ -439,10 +448,12 @@ const Ge = ({
             </Step>
             <Step
               active={getsessionactive(3)}
-              completed={getsessioncomplete(3)}>
+              completed={getsessioncomplete(3)}
+            >
               <StepLabel
                 StepIconComponent={LiveTvIcon}
-                sx={{ backgroundColor: timeline > 3 ? "#58eb34" : "" }}>
+                sx={{ backgroundColor: timeline > 3 ? "#58eb34" : "" }}
+              >
                 <h6>
                   {lang == "th"
                     ? "ประกาศผลอย่างเป็นทางการ"
@@ -456,9 +467,201 @@ const Ge = ({
           </Stepper>
         </Box>
 
+        <Box className="mt-5">
+          <CardHeader title="Begining of War" />
+          <div className="container" data-aos="fade-right">
+            <Card>
+              <CardMedia
+                sx={{ height: "50vh", width: "100%" }}
+                image="https://pbs.twimg.com/media/Ge0Am9Vb0AE5pVe?format=jpg&name=large"
+                title="kfge5begin"
+              />
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {lang == "th"
+                    ? "ข้าวฟ่าง–ญาณิศา เมืองคำ (BNK48 Team NV) ได้ลงสมัคร BNK48 & CGM48 Senbatsu General Election 2025 เมื่อวันที่ 15 ธันวาคม 2024 เวลา 00:25 (ตามเวลาประเทศไทย)"
+                    : "Kaofrang Yanisa or Kaofrang BNK48 has applied for joining BNK48 & CGM48 Senbatsu General Election 2025 in December 15, 2024 about 0:50 AM. (Based on Asia/Bangkok Timezone)"}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      "https://www.facebook.com/bnk48official/posts/pfbid0JXgFZzmA6CLm9wx9cucESrgSZYk1qv8Yw1ZsoPe4EmkxuQJyL4FPLv8XfzoLmGqMl",
+                      "_blank"
+                    )
+                  }
+                >
+                  {lang == "th" ? "ไปยังลิงก์" : "Go to external link"}
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+        </Box>
+
+        <Box className="mt-5">
+          <CardHeader
+            title="The Songs List of Single (BNK48 19th Single)"
+            subheader={
+              lang == "th"
+                ? "เพลงที่ข้าวฟ่างเลือกเป็นเซ็นเตอร์"
+                : "Song list for Center position of Kaofrang"
+            }
+          />
+          <div className="container">
+            <Card data-aos="fade-right">
+              <CardMedia
+                sx={{ height: "50vh", width: "100%" }}
+                src="https://youtube.com/embed/CGXwRIcnrJo"
+                component="iframe"
+              />
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {lang == "th"
+                    ? "เพลงหลัก (อันดับที่ 1) - Saikou ka yo | HKT48"
+                    : "Main song (1st Position) - Saikou ka yo | HKT48"}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      "https://youtube.com/watch?v=CGXwRIcnrJo",
+                      "_blank"
+                    )
+                  }
+                >
+                  {lang == "th"
+                    ? "รับชมมิวสิควีดีโอต้นฉบับ"
+                    : "Watching original Music Video"}
+                </Button>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      "https://open.spotify.com/track/6wgJfy5bVOhEiKz08YaV64",
+                      "_blank"
+                    )
+                  }
+                >
+                  {lang == "th" ? "รับฟังบน Spotify" : "Listen it on Spotify!"}
+                </Button>
+              </CardActions>
+            </Card>
+            <Card className="mt-3" data-aos="fade-right">
+              <CardMedia
+                sx={{ height: "50vh", width: "100%" }}
+                src="https://youtube.com/embed/0pKfxbCHLoU"
+                component="iframe"
+              />
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {lang == "th"
+                    ? "เพลงรอง (อันดับที่ 13) - Sustainable | AKB48"
+                    : "Couple song (13th Position) - Sustainable | AKB48"}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      "https://youtube.com/watch?v=0pKfxbCHLoU",
+                      "_blank"
+                    )
+                  }
+                >
+                  {lang == "th"
+                    ? "รับชมมิวสิควีดีโอต้นฉบับ"
+                    : "Watching original Music Video"}
+                </Button>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      "https://open.spotify.com/track/1Paki9ZUoGAJCDfykNrHV8",
+                      "_blank"
+                    )
+                  }
+                >
+                  {lang == "th" ? "รับฟังบน Spotify" : "Listen it on Spotify!"}
+                </Button>
+              </CardActions>
+            </Card>
+            <Card className="mt-3" data-aos="fade-right">
+              <CardMedia
+                sx={{ height: "50vh", width: "100%" }}
+                src="https://youtube.com/embed/tBFJFAP3GKU"
+                component="iframe"
+              />
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {lang == "th"
+                    ? "เพลงรอง (อันดับที่ 25) - Idol Nanka Janakattara | AKB48"
+                    : "Couple song (25th Position) - Idol Nanka Janakattara | AKB48"}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      "https://youtube.com/watch?v=tBFJFAP3GKU",
+                      "_blank"
+                    )
+                  }
+                >
+                  {lang == "th"
+                    ? "รับชมมิวสิควีดีโอต้นฉบับ"
+                    : "Watching original Music Video"}
+                </Button>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      "https://open.spotify.com/track/0svM1S2Msb3aIfpf2Cf0YT",
+                      "_blank"
+                    )
+                  }
+                >
+                  {lang == "th" ? "รับฟังบน Spotify" : "Listen it on Spotify!"}
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+        </Box>
+
+        <Box className="mt-5">
+          <CardHeader title="Special Project for Kami 7" />
+          <div className="container" data-aos="fade-right">
+            <Card>
+              <CardMedia
+                sx={{ height: "100vh", width: "100%" }}
+                image="https://i.scdn.co/image/ab6761610000e5ebcf6dc7909f08fd4c42c59a24"
+                title="fhero"
+              />
+              <CardContent>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {lang == "th"
+                    ? "7 เมมเบอร์ที่ได้รับคะแนนโหวตสูงที่สุด จะมีโอกาสได้ทำโปรเจคพิเศษร่วมกับ F.HERO (กอล์ฟ ฟักกลิ้งฮีโร่ หรือณัฐวุฒิ ศรีหมอก)"
+                    : "The 7 members that received the highest votes will have the opportunity to do a special project with F.HERO (Nattawut Srimhok)"}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  onClick={() =>
+                    window.open(
+                      "https://youtu.be/1cVvscOjruc",
+                      "_blank"
+                    )
+                  }
+                >
+                  {lang == "th" ? "ไปยังลิงก์" : "Go to external link"}
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+        </Box>
+
         <Backdrop
           sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-          open={load}>
+          open={load}
+        >
           <CircularProgress />
         </Backdrop>
       </Box>
